dbf72b45306a875daa9533eb1dc49df278b7e428
shurupov <bolteg86@ya.ru>
2022-01-11 20:37:51 +0400

    HomeWork-1

---
+++ hw1/HelloWorld2/HelloWorld2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!");
+            Console.ReadKey();
+        }
+    }
+}
