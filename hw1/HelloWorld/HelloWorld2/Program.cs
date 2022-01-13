5185bc7075ddab5d15d146261702b812684daa57
shurupov <bolteg86@ya.ru>
2022-01-13 23:12:11 +0400

    Solution refactoring

---
+++ hw1/HelloWorld/HelloWorld2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!");
+            Console.ReadKey();
+        }
+    }
+}
