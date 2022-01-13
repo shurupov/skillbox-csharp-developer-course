82248879f4bb0ff27f0223695b0730bd33de8f1d
shurupov <bolteg86@ya.ru>
2022-01-14 00:21:44 +0400

    homework-2

---
+++ homework-02/task-01-types-formatting-operations/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace task_01_types
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string fullName = "Пушкин Александр Сергеевич";
+            int age = 37;
+            string email = "pushkin@lycey.ru";
+            int programmingScores = 99;
+            int mathScores = 77;
+            int physicsScores = 89;
+            
+            Console.WriteLine($"{"ФИО:", 26} {fullName}");
+            Console.WriteLine($"{"Возраст:", 26} {age}");
+            Console.WriteLine($"{"Email:", 26} {email}");
+            Console.WriteLine($"{"Баллы по программированию:", 26} {programmingScores}");
+            Console.WriteLine($"{"Баллы по математике:", 26} {mathScores}");
+            Console.WriteLine($"{"Баллы по физике:", 26} {physicsScores}");
+
+            Console.ReadKey();
+
+            double average = (double) (programmingScores + mathScores + physicsScores) / 3;
+            
+            Console.WriteLine($"{"Средний балл:", 26} {average}");
+        }
+    }
+}
