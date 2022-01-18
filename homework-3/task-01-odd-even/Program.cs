8e23280dc2d18fd7a87c2d2f87062e8281750f77
Evgeny Shurupov <shurupovea@elocont.ru>
2022-01-18 20:51:46 +0400

    homework-3

---
+++ homework-3/task-01-odd-even/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task_01_add_even
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число");
+            uint number = uint.Parse(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Число чётное");
+            }
+            else
+            {
+                Console.WriteLine("Число нечётное");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ homework-3/task-02-blackjack/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace task_02_blackjack
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Сколько у вас карт");
+            uint cardsCount = uint.Parse(Console.ReadLine());
+            uint points = 0;
+            for (uint i = 0; i < cardsCount; i++)
+            {
+                Console.WriteLine("Введите номинал карты (J, Q, K, A, 2-10)");
+                string card = Console.ReadLine();
+                switch (card)
+                {
+                    case "J" :
+                    case "Q" :
+                    case "K" :
+                        points += 10;
+                        break;
+                    case "A" :
+                        points += 11;
+                        break;
+                    default:
+                        points += uint.Parse(card);
+                        break;
+                }
+            }
+            Console.WriteLine($"Вы набрали {points} очков");
+        }
+    }
+}
