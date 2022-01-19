525b1ee763bf76440de28fada3097350250573fe
Evgeny Shurupov <shurupovea@elocont.ru>
2022-01-19 18:39:32 +0400

    homework-3

---
+++ homework-3/task-03-prime-number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task_03_prime_number
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            uint number = uint.Parse(Console.ReadLine());
+            bool isPrime = true;
+            for (uint i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            Console.WriteLine("Введённое число " + (isPrime ? "простое" : "не простое"));
+            
+            
+        }
+    }
+}
