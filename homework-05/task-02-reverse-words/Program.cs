91484d5fa3fdbda50dbf8362b965d86455766043
shurupov <bolteg86@ya.ru>
2022-03-09 00:34:32 +0400

    homework-05

---
+++ homework-05/task-02-reverse-words/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace task_02_reverse_words
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence:");
+            string line = Console.ReadLine();
+            string reversed = ReversWords(line);
+            Console.WriteLine(reversed);
+        }
+
+        private static string ReversWords(string inputPhrase)
+        {
+            string result = "";
+            string[] words = Split(inputPhrase);
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                result += words[i] + ' ';
+            }
+
+            return result;
+        }
+        
+        private static string[] Split(string line)
+        {
+            return line.Split(' ');
+        }
+    }
+}
